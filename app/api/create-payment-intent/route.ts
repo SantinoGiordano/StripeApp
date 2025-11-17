690766539dde26a131b4f58a43c352844783cf16
SantinoGiordano <santinogiordano13@gmail.com>
2025-11-17 11:58:31 -0500

    logging the amount and the cart for debugging

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
-
 import Stripe from "stripe";
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-06-30.basil",
 });
@@ -8,13 +8,21 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 export async function POST(request: NextRequest) {
   try {
     const { amount, cart } = await request.json();
+    console.log("Creating payment intent with amount:", amount);
+    console.log("Creating payment intent with amount:", cart);
+
+    // vLDATE CART OBJECT EXISTS
+    // TOD DO NOT TRUST AMOUNT FGROM FRONtENT 
+    // LOOP OVER CART ITEMS AND ADD THEM UP YOURSELF
+    // MAKE DB REQUEST, LIST ALL ITEMS FROM DB -> GET PRICCES -> CALCULATE TOTAL
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "usd",
       automatic_payment_methods: { enabled: true },
+      
       metadata: {
-        cart: JSON.stringify(cart),
+        cart,
       },
     });
 
