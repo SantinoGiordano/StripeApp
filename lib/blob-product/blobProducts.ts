d1907813372463554044abcacc4a97898dd420f5
SantinoGiordano <santinogiordano13@gmail.com>
2025-12-02 10:46:20 -0500

    changed name of blob Product to be tracked on git and server

---
+++ lib/blob-product/blobProducts.ts
@@ -0,0 +1,20 @@
+export const BLOB_PRODUCTS = [
+  {
+    id: "687fa4a7d59f9a593b7dad10",
+    name: "Meditation Audio",
+    downloadUrl:
+      "https://gidpqrp8kmdnhzy1.public.blob.vercel-storage.com/.PRODUCTS/687fa4a7d59f9a593b7dad10.mp3?download=1",
+  },
+  {
+    id: "687fa4e9d59f9a593b7dad12",
+    name: "Sleep Audio",
+    downloadUrl:
+      "https://gidpqrp8kmdnhzy1.public.blob.vercel-storage.com/.PRODUCTS/687fa4e9d59f9a593b7dad12.mp3?download=1",
+  },
+  {
+    id: "687fa46ad59f9a593b7dad0e",
+    name: "Stress Relief Meditation",
+    downloadUrl:
+      "https://gidpqrp8kmdnhzy1.public.blob.vercel-storage.com/.PRODUCTS/687fa46ad59f9a593b7dad0e.mp3?download=1",
+  },
+];
